51c6972ae9b83b60fe60d10d88b5c1d8a323c946
Vyacheslav Zamyatin <vyzamyatin@gmail.com>
2023-04-10 12:27:00 +0300

    [DE] optimizations.sql (part 2)

--- db/optimizations.sql
+++ db/optimizations.sql
@@ -1,1 +1,2 @@
 create index concurrently posts_rootid_createat_partial on posts using btree(rootid, createat) where coalesce(deleteat, 0) = 0;
+create index concurrently idx_posts_message_txt2 on posts using gin(to_tsvector('russian'::regconfig, message::text));
